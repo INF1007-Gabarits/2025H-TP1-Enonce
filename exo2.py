75b840e117a96d0563ec96f0a1426d1f5755aac0
Othmane0418 <othmane.harbi@polymtl.ca>
2025-01-22 15:56:39 -0500

    exo4 done

--- exo2.py
+++ exo2.py
@@ -1,11 +1,13 @@
 # Demandez à l'utilisateur d'entrer le niveau de charge actuel de la batterie
-
+charge = int(input("Veuillez entrer le niveau de charge actuel de la batterie"))
 # Vérifiez si le niveau de charge est valide
+if charge < 0 or charge > 100
+int(input("Veuillez entrer le niveau de charge actuel de la batterie"))
 
 # Arrondir le pourcentage à la dizaine la plus proche
-
+pourcentage = round (charge,charge%100 == 0)
 # Calculer le nombre de barres
-
+nombre de barres = int(pourcentage /10)
 # Afficher la représentation graphique de la charge de la batterie
 
 # Exemple d'utilisation :
